d6182f686094998ef6a6d4af26b83ef1937e4f67
Matthew Wever <102130@glr.nl>
2025-01-16 15:17:41 +0000

    Update 3

---
+++ Schip Details Page ( nieuwe versie ) pg 3/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.navbar__toggle');
+    const dropdownToggle = document.querySelector('.dropdown');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    menuToggle.addEventListener('click', function() {
+        dropdownContent.classList.toggle('active');
+        menuToggle.classList.toggle('open');
+    });
+
+    dropdownToggle.addEventListener('click', function() {
+        dropdownContent.classList.toggle('active');
+        dropdownToggle.classList.toggle('open');
+    });
+});
